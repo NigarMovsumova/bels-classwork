8038f933fa46b2ed255a583b88af5dfe19a9dc63
NigarMovsumova <nigar.movsumova@pashabank.az>
2019-09-23 15:26:53 +0400

    Initial commit

---
+++ src/main/java/az/pashabank/ls/belsclasswork/BelsClassworkApplication.java
@@ -0,0 +1,13 @@
+package az.pashabank.ls.belsclasswork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelsClassworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BelsClassworkApplication.class, args);
+    }
+
+}
